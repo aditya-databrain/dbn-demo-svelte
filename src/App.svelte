bd59573fe54de09f8300cb984e32a1907e0763d2
Aditya Yaduvanshi <aditya@usedatabrain.com>
2023-06-09 13:04:59 +0530

    init

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import 'dbn-ui-test-1/web'
+</script>
+
+<main>
+  <dbn-dashboard
+    token="f2834487-d196-463c-9a57-9e02f78c7dda"
+    dashboardId="dev-orders"
+  />
+</main>
+
+<style>
+</style>
