eda2d7f6ba323a5c13f609c61a09112ad784ebe4
Aditya Yaduvanshi <aditya@usedatabrain.com>
2023-06-15 15:51:20 +0530

    integrate actual plugin

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
-import 'dbn-ui-test-1/web'
+import '@databrainhq/plugin/web';
+const urlParams = new URLSearchParams(window.location.search);
+const token = urlParams.get('token');
+const dashboardId = urlParams.get('dashboardId');
 </script>
 
 <main>
   <dbn-dashboard
-    token="f2834487-d196-463c-9a57-9e02f78c7dda"
-    dashboardId="dev-orders"
+    token={token}
+    dashboardId={dashboardId}
   />
 </main>
 
 <style>
-  main {
-    width: 100vw;
-    height: 100vh;
-    position: relative;
-  }
 </style>
